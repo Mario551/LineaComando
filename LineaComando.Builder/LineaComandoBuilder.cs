a9d729ea4a46ac7b3d70e0d3ab29d325c0d8bf51
Mario <12356746+Mario551@users.noreply.github.com>
2025-12-31 20:47:52 -0500

    Configurador comandos en función ConCola y Configurador eventos en función ConEventDriven

--- LineaComando.Builder/LineaComandoBuilder.cs
+++ LineaComando.Builder/LineaComandoBuilder.cs
@@ -1,5 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PER.Comandos.LineaComandos.Cola.Almacen;
+using PER.Comandos.LineaComandos.EventDriven.Registro;
+using PER.Comandos.LineaComandos.Registro;
 
 namespace PER.Comandos.LineaComandos.Builder
 {
@@ -15,6 +18,9 @@ namespace PER.Comandos.LineaComandos.Builder
         internal TimeSpan TiempoRefrescoTareas { get; private set; } = TimeSpan.FromSeconds(1);
         internal int MaxParalelismoCola { get; private set; } = 4;
 
+        internal Func<IServiceProvider, IRegistroComandos<string, ResultadoComando>, CancellationToken, Task>? ConfigurarComandos { get; private set; }
+        internal Func<IServiceProvider, IRegistroTiposEvento, CancellationToken, Task>? ConfigurarEventos { get; private set; }
+
         internal string ConnectionString => _connectionString;
         internal IServiceCollection Services => _services;
 
@@ -25,10 +31,12 @@ namespace PER.Comandos.LineaComandos.Builder
         }
 
         public LineaComandoBuilder ConColaComandos(
+            Func<IServiceProvider, IRegistroComandos<string, ResultadoComando>, CancellationToken, Task> configurarComandos,
             TimeSpan? tiempoRefresco = null,
             int maxParalelismo = 4)
         {
             UsarCola = true;
+            ConfigurarComandos = configurarComandos;
             if (tiempoRefresco.HasValue)
                 TiempoRefrescoCola = tiempoRefresco.Value;
             MaxParalelismoCola = maxParalelismo;
@@ -36,10 +44,12 @@ namespace PER.Comandos.LineaComandos.Builder
         }
 
         public LineaComandoBuilder ConEventDriven(
+            Func<IServiceProvider, IRegistroTiposEvento, CancellationToken, Task> configurarEventos,
             TimeSpan? tiempoRefrescoEventos = null,
             TimeSpan? tiempoRefrescoTareas = null)
         {
             UsarEventDriven = true;
+            ConfigurarEventos = configurarEventos;
             if (tiempoRefrescoEventos.HasValue)
                 TiempoRefrescoEventos = tiempoRefrescoEventos.Value;
             if (tiempoRefrescoTareas.HasValue)
--- LineaComando.Builder/LineaComandoExtensions.cs
+++ LineaComando.Builder/LineaComandoExtensions.cs
@@ -1,7 +1,10 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PER.Comandos.LineaComandos.Cola.Almacen;
 using PER.Comandos.LineaComandos.Cola.Esquema;
 using PER.Comandos.LineaComandos.EventDriven.Esquema;
+using PER.Comandos.LineaComandos.EventDriven.Registro;
+using PER.Comandos.LineaComandos.Registro;
 
 namespace PER.Comandos.LineaComandos.Builder
 {
@@ -24,12 +27,24 @@ namespace PER.Comandos.LineaComandos.Builder
             {
                 var inicializadorCola = new InicializadorEsquema(builder.ConnectionString);
                 await inicializadorCola.InicializarAsync(token);
+
+                if (builder.ConfigurarComandos != null)
+                {
+                    var registroComandos = host.Services.GetRequiredService<IRegistroComandos<string, ResultadoComando>>();
+                    await builder.ConfigurarComandos(host.Services, registroComandos, token);
+                }
             }
 
             if (builder.UsarEventDriven)
             {
                 var inicializadorEventDriven = new InicializadorEsquemaEventDriven(builder.ConnectionString);
                 await inicializadorEventDriven.InicializarAsync(token);
+
+                if (builder.ConfigurarEventos != null)
+                {
+                    var registroTiposEvento = host.Services.GetRequiredService<IRegistroTiposEvento>();
+                    await builder.ConfigurarEventos!(host.Services, registroTiposEvento, token);
+                }
             }
         }
     }
